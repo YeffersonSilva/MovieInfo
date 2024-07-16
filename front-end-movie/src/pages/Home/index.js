f13ad71ec8804c9a338eb244e65d03f7ce07139b
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-07-16 20:23:24 -0300

    feat: fetch data from API

--- front-end-movie/src/pages/Home/index.js
+++ front-end-movie/src/pages/Home/index.js
@@ -1,11 +1,39 @@
+import api from "../../services/api";
+import { useEffect, useState } from "react";
+
 function Home() {
-    return (
-        <div>
-            <h2>Home</h2>
-       </div>
+  const [filmes, setFilmes] = useState([]);
 
-    )
+  useEffect(() => {
+    async function loadFilmes() {
+      try {
+        const response = await api.get("movie/now_playing", {
+          params: {
+            api_key:process.env.REACT_APP_API_KEY,
+            language: "pt-BR",
+            page: 1,
+          },
+        });
+          setFilmes(response.data.results);
+          console.log(response.data.results);
+      } catch (error) {
+        console.error("Request failed with status code", error.response.status);
+      }
+    }
+    loadFilmes();
+  }, []);
+
+  return (
+    <div>
+      <h2>Home</h2>
+      {filmes.map(filme => (
+        <div key={filme.id}>
+          <h3>{filme.title}</h3>
+          <p>{filme.overview}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
-
 
 export default Home;
